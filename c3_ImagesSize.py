281ef8100b10f6a1ecadf198594515b9dce8cfdd
BloodShop <koliakovcr7@gmail.com>
2022-09-11 18:29:14 +0300

    .. img

--- c3_ImagesSize.py
+++ c3_ImagesSize.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-img = cv2.imread("../Resources/lambo.jpg")
+img = cv2.imread("/Resources/lambo.jpg")
 print(img.shape)
 
 imgResize = cv2.resize(img,(300,200)) # Width => Height
